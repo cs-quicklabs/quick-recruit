1a85ab46f07051677d6fdae6d9cf3d1b721198af
Aashish Dhawan <aashishdhawan@gmail.com>
2024-01-18 19:26:44 +0530

    partial login setup

---
+++ app/controllers/registeration_controller.rb
@@ -0,0 +1,21 @@
+class RegisterationController < ApplicationController
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(registeration_params)
+        if @user.save
+            login @user
+            redirect_to root_path, notice: "You have been registered."
+        else 
+            render :new, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def registeration_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end

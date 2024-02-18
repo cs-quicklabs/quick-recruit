329387dbc8153bdbf10524a1e84f3faef654f977
Aashish Dhawan <aashishdhawan@gmail.com>
2024-02-18 18:29:20 +0530

    Added opening

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -1,2 +1,21 @@
 class OpeningsController < ApplicationController
+  def index
+    @openings = Opening.all
+  end
+
+  def create
+    @opening = Opening.create(openings_params)
+    @opening.save
+    redirect_to openings_path, notice: "New opening was created successfully"
+  end
+
+  def new
+    @opening = Opening.new
+  end
+
+  private
+
+  def openings_params
+    params.require(:opening).permit(:title)
+  end
 end

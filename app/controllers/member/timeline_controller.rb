10198a5e209a1781c1497739bfc8c0756a344dcd
Aashish Dhawan <aashishdhawan@gmail.com>
2024-03-15 22:16:38 +0530

    update etag

--- app/controllers/member/timeline_controller.rb
+++ app/controllers/member/timeline_controller.rb
@@ -2,6 +2,8 @@ class Member::TimelineController < Member::BaseController
   def index
     def index
       @events = Event.where(user: @member).order(created_at: :desc).limit(50)
+      
+      fresh_when @events
     end
   end
 end

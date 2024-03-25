d74814ca6ce13902465d7947861ae754b42d369a
Aashish Dhawan <aashishdhawan@gmail.com>
2024-03-25 12:44:06 +0530

    updated earch

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,13 @@
 class SearchController < BaseController
   def candidates
-    query = "%#{params[:q]}%".split(/\s+/)
+    query = "%#{params[:q]}%"
     @candidates = candidates_matching_query(query)
     render layout: false
   end
 
   def candidates_matching_query(like_keyword)
-    Candidate.where("first_name iLIKE ANY ( array[?] )", like_keyword)
-      .or(Candidate.where("last_name iLIKE ANY ( array[?] )", like_keyword))
-      .or(Candidate.where("email iLIKE ANY ( array[?] )", like_keyword))
+    Candidate.where("CONCAT_WS(' ', first_name, last_name) iLIKE ?", like_keyword)
+      .or(Candidate.where("email iLIKE ?", like_keyword))
       .order(:first_name).limit(7)
   end
 end

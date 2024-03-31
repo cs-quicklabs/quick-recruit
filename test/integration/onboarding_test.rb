bf6779c198d97bf4af129560b4e4a11be7eac68a
Aashish Dhawan <aashishdhawan@gmail.com>
2024-03-31 09:37:14 +0530

    Added onboarding integration test

---
+++ test/integration/onboarding_test.rb
@@ -0,0 +1,66 @@
+require "test_helper"
+
+class OnboardingTest < ActionDispatch::IntegrationTest
+
+  # Dashboard
+  test "admin can sign in" do
+    user = users(:admin)
+
+    get new_session_url
+    assert_response :success
+
+    post session_url, params: { email: user.email, password: "password" }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", "You have been logged in."
+  end
+
+  test "recruiter can sign in" do
+    user = users(:recruiter)
+
+    get new_session_url
+    assert_response :success
+
+    post session_url, params: { email: user.email, password: "password" }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", "You have been logged in."
+  end
+
+  test "data can sign in" do
+    user = users(:data)
+
+    get new_session_url
+    assert_response :success
+
+    post session_url, params: { email: user.email, password: "password" }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", "You have been logged in."
+  end
+
+  test "interviewer can sign in" do
+    user = users(:interviewer)
+
+    get new_session_url
+    assert_response :success
+
+    post session_url, params: { email: user.email, password: "password" }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", "You have been logged in."
+  end
+
+  test "wrong credentials can not sign in" do
+    get new_session_url
+    assert_response :success
+
+    post session_url, params: { email: "random@gmail.com", password: "random" }
+    assert_response :unprocessable_entity
+    assert_select "p", "Your email or password is incorrect."
+  end
+end

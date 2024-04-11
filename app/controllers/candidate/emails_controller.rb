d43a1360c5bc3a1b25efd198a2a44ba4b1d68860
Aashish Dhawan <aashishdhawan@gmail.com>
2024-04-11 09:45:12 +0530

    fresh_when on email

--- app/controllers/candidate/emails_controller.rb
+++ app/controllers/candidate/emails_controller.rb
@@ -1,14 +1,15 @@
 class Candidate::EmailsController < Candidate::BaseController
   def index
     @emails = @candidate.emails.order(created_at: :desc)
+    fresh_when [@candidate] + @emails
   end
 
   def create
     email = SendEmail.call(@candidate, params[:kind], current_user).result
     respond_to do |format|
-        if email.persisted?
-            format.turbo_stream { render turbo_stream: turbo_stream.prepend(:emails, partial: "candidate/emails/email", locals: { email: email }) }
-        end
+      if email.persisted?
+        format.turbo_stream { render turbo_stream: turbo_stream.prepend(:emails, partial: "candidate/emails/email", locals: { email: email }) }
+      end
     end
   end
 end

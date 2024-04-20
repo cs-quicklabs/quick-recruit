c6d59a9c6ddff5fa51b9198af51a856865f085b0
Aashish Dhawan <aashishdhawan@gmail.com>
2024-04-20 16:25:03 +0530

    Added default URL options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => "recruit.quicklabs.in" }
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     "recruit.quicklabs.in",     # Allow requests from example.com

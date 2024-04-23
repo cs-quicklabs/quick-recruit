41e051ed005c24970790a42c649c4de21c5c3a8a
Aashish Dhawan <aashishdhawan@gmail.com>
2024-04-23 21:41:34 +0530

    emails

--- app/mailers/candidate_mailer.rb
+++ app/mailers/candidate_mailer.rb
@@ -1,16 +1,19 @@
 class CandidateMailer < ApplicationMailer
   def rejection_email
-    @candidate = params[:candidate]
-    mail(to: [@candidate.email, @candidate.owner.email], subject: "Hiring@Crownstack: Interview Result", reply_to: @candidate.owner.email)
+    candidate = params[:candidate].email
+    owner = candidate.owner.email
+    mail(to: [@candidate, owner], from: owner, subject: "Hiring@Crownstack: Interview Result", reply_to: candidate)
   end
 
   def about_us_email
-    @candidate = params[:candidate]
-    mail(to: [@candidate.email, @candidate.owner.email], subject: "Hiring@Crownstack: Company Profile", reply_to: @candidate.owner.email)
+    candidate = params[:candidate].email
+    owner = candidate.owner.email
+    mail(to: [candidate, owner], from: owner, subject: "Hiring@Crownstack: Company Profile", reply_to: candidate)
   end
 
   def job_description_email
-    @candidate = params[:candidate]
-    mail(to: [@candidate.email, @candidate.owner.email], subject: "Hiring@Crownstack: Job Description", reply_to: @candidate.owner.email)
+    candidate = params[:candidate].email
+    owner = candidate.owner.email
+    mail(to: [candiate, owner], from: owner, subject: "Hiring@Crownstack: Job Description", reply_to: candidate)
   end
 end

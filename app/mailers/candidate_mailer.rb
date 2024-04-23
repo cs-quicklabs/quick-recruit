fbac3f5f6d30dbdf92242b736a91637886373b24
Aashish Dhawan <aashishdhawan@gmail.com>
2024-04-23 21:54:43 +0530

    Added email fixes

--- app/mailers/candidate_mailer.rb
+++ app/mailers/candidate_mailer.rb
@@ -1,19 +1,19 @@
 class CandidateMailer < ApplicationMailer
   def rejection_email
-    candidate = params[:candidate].email
-    owner = candidate.owner.email
-    mail(to: [candidate, owner], from: owner, subject: "Hiring@Crownstack: Interview Result", reply_to: candidate)
+    @candidate = params[:candidate]
+    @owner = params[:candidate].owner
+    mail(to: [@candidate.email, @owner.email], from: @owner.email, subject: "Hiring@Crownstack: Interview Result", reply_to: @candidate.email)
   end
 
   def about_us_email
-    candidate = params[:candidate].email
-    owner = candidate.owner.email
-    mail(to: [candidate, owner], from: owner, subject: "Hiring@Crownstack: Company Profile", reply_to: candidate)
+    @candidate = params[:candidate]
+    @owner = params[:candidate].owner
+    mail(to: [@candidate.email, @owner.email], from: @owner.email, subject: "Hiring@Crownstack: Company Profile", reply_to: @candidate.email)
   end
 
   def job_description_email
-    candidate = params[:candidate].email
-    owner = candidate.owner.email
-    mail(to: [candiate, owner], from: owner, subject: "Hiring@Crownstack: Job Description", reply_to: candidate)
+    @candidate = params[:candidate]
+    @owner = params[:candidate].owner
+    mail(to: [@candidate.email, @owner.email], from: @owner.email, subject: "Hiring@Crownstack: Job Description", reply_to: @candidate.email)
   end
 end

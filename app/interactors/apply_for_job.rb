45716106310a3d3ed30eeb789d0b3d7c0db40a3a
Aashish Dhawan <aashishdhawan@gmail.com>
2024-05-25 18:57:06 +0530

    Updated Apply form

--- app/interactors/apply_for_job.rb
+++ app/interactors/apply_for_job.rb
@@ -7,6 +7,15 @@ class ApplyForJob < Patterns::Service
     @phone = params[:phone]
     @biography = params[:biography]
     @opening_id = params[:opening_id]
+    @location = params[:location]
+    @birth_year = params[:birth_year]
+    @current_company = params[:current_company]
+    @current_title = params[:current_title]
+    @current_ctc = params[:current_ctc]
+    @expected_ctc = params[:expected_ctc]
+    @notice_period = params[:notice_period]
+    @experience = params[:experience]
+    @highest_qualification = params[:highest_qualification]
   end
 
   def call
@@ -23,11 +32,17 @@ class ApplyForJob < Patterns::Service
   def create_candidate
     #Opening becomes "to Be Decided" if form is submitted without selecting an opening
     opening = opening_id.blank? ? Opening.find(20) : Opening.find(opening_id)
-    Candidate.create!(first_name: first_name, last_name: last_name, email: email, phone: phone, biography: biography, opening_id: opening.id, role: opening.role, bucket: Candidate.buckets[:leads], user: User.bot, owner: opening.owner, status: Candidate.statuses[:waiting_for_evaluation], source: Source.find_by_title("Website"))
+
+    Candidate.create!(first_name: first_name, last_name: last_name, email: email, phone: phone, biography: biography, opening_id: opening.id, role: opening.role, bucket: Candidate.buckets[:leads], user: User.bot, owner: opening.owner, status: Candidate.statuses[:waiting_for_evaluation], source: Source.find_by_title("Website"), location: location, birth_year: birth_year, current_company: current_company, current_title: current_title, current_ctc: current_ctc, expected_ctc: expected_ctc, notice_period: notice_period, experience: experience, highest_qualification: highest_qualification)
   end
 
   def existing_candidate
-    Candidate.find_by_email(email) or Candidate.find_by_phone(phone)
+    candidate = Candidate.find_by_email(email) or Candidate.find_by_phone(phone)
+
+    # update old info to newest one
+    candidate.update(first_name: first_name, last_name: last_name, biography: biography, location: location, birth_year: birth_year, current_company: current_company, current_title: current_title, current_ctc: current_ctc, expected_ctc: expected_ctc, notice_period: notice_period, experience: experience, highest_qualification: highest_qualification) if candidate.persisted?
+
+    candidate
   end
 
   def update_bucket(candidate)
@@ -38,5 +53,5 @@ class ApplyForJob < Patterns::Service
     CandidateMailer.with(content: params).lead_email.deliver_later
   end
 
-  attr_reader :first_name, :last_name, :email, :phone, :biography, :opening_id, :params
+  attr_reader :first_name, :last_name, :email, :phone, :biography, :opening_id, :location, :birth_year, :current_title, :current_company, :current_ctc, :expected_ctc, :notice_period, :experience, :highest_qualification, :params
 end

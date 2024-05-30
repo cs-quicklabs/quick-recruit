a3e6ea024aea334bdadb98b2a690c232a72f52a8
Aashish Dhawan <aashishdhawan@gmail.com>
2024-05-30 22:44:29 +0530

    show 50 events on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class DashboardController < BaseController
   def events
-    @events = Event.includes(:eventable, :trackable, :user).order(created_at: :desc).limit(50)
+    @events = Event.includes(:eventable, :trackable, :user).where(action: notificable_actions).order(created_at: :desc).limit(50)
+  end
+
+  private
+
+  def notificable_actions
+    # show only selected actions in the dashboard
+    ["update_bucket", "add_note", "add_report", "add_candidate"]
   end
 end

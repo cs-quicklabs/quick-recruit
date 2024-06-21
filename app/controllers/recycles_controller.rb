79348da31476f97de3599a87acb09175bb0c2323
Aashish Dhawan <aashishdhawan@gmail.com>
2024-06-21 07:52:56 +0530

    update recycle permissions

--- app/controllers/recycles_controller.rb
+++ app/controllers/recycles_controller.rb
@@ -1,9 +1,25 @@
 class RecyclesController < BaseController
   def index
-    @recycles = Recycle.includes(candidate: [:user, :role, :opening, :owner])
-    if params[:recruiter].present?
-      @recycles = @recycles.select { |recycle| recycle.candidate.owner.id == params[:recruiter].to_i }
-    end
+    @recycles = get_recycles
     @recruiters = User.recruiters
+  end
+
+  private
+
+  def recycles_for_admin
+    recycles = Recycle.includes(candidate: [:user, :role, :opening, :owner])
+    if params[:recruiter].present?
+      recycles = recycles.select { |recycle| recycle.candidate.owner.id == params[:recruiter].to_i }
+    end
+
+    recycles
+  end
+
+  def recycles_for_recruiter
+    Recycle.includes(candidate: [:user, :role, :opening, :owner]).where(candidate: { owner: current_user })
+  end
+
+  def get_recycles
+    current_user.admin? ? recycles_for_admin : recycles_for_recruiter
   end
 end

27894a52f3fd147b1a19cdbb4854415a15a18d45
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-06 21:37:49 +0530

    linkedin url fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,12 @@ module ApplicationHelper
   def auto_link_urls_in_text(text)
     auto_link(text, html: { class: "text-indigo-700 hover:underline", target: "_blank" })
   end
+
+  def absolute_url(path)
+    if path.include?("https://") || path.include?("http://")
+      path
+    else
+      path.prepend("https://")
+    end
+  end
 end

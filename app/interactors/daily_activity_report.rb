5aba3685d7d8d8fc99f17ae9fd467f3b38b5fba8
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-09 00:24:40 +0530

    fixed bucket issues in report

--- app/interactors/daily_activity_report.rb
+++ app/interactors/daily_activity_report.rb
@@ -21,7 +21,7 @@ class DailyActivityReport < Patterns::Service
   end
 
   def updated_candidates_count(recruiter)
-    params = { created_by: recruiter.id.to_s, updated_after_date: Date.yesterday.to_formatted_s(:db), updated_before_date: Date.yesterday.to_formatted_s(:db) }
+    params = { created_by: recruiter.id.to_s, updated_after_date: Date.yesterday.to_formatted_s(:db), updated_before_date: Date.yesterday.to_formatted_s(:db), bucket: :pipeline }
 
     Candidate.update_candidates_query(params).count
   end

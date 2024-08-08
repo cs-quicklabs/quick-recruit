42b91a6be44bed4dc68872df213aca8e29ed7c28
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-08 15:30:49 +0530

    minor changes in logic

--- app/interactors/recycle_candidates.rb
+++ app/interactors/recycle_candidates.rb
@@ -71,7 +71,7 @@ class RecycleCandidates < Patterns::Service
   end
 
   def count_of_new_profiles_needed_for(recruiter, opening)
-    total_quota_per_recruiter = 10 # 80 profiles per recruiter
+    total_quota_per_recruiter = 10 # 10 profiles per recruiter
     quota_per_opening = total_quota_per_recruiter / recruiter.openings.count
     existing_profiles = Recycle.includes(candidate: [:user, :role, :opening, :owner]).where(candidate: { owner_id: recruiter.id, role_id: opening.role_id }).count
     count = quota_per_opening - existing_profiles
@@ -80,7 +80,7 @@ class RecycleCandidates < Patterns::Service
   end
 
   def count_of_profiles_needed_for_to_be_decided(recruiter)
-    total_quota_per_recruiter = 10 # 80 profiles per recruiter
+    total_quota_per_recruiter = 10 # 10 profiles per recruiter
     existing_profiles = Recycle.includes(candidate: [:user, :role, :opening, :owner]).where(candidate: { owner_id: recruiter.id }).count
     count = total_quota_per_recruiter - existing_profiles
     count = count.positive? ? count : 0
@@ -88,17 +88,17 @@ class RecycleCandidates < Patterns::Service
   end
 
   def recycle_champions_for(recruiter, opening, count)
-    candidates = Candidate.where(bucket: "champions", role_id: opening.role_id).where("bucket_updated_on < ? ", 6.months.ago)
+    candidates = Candidate.where(bucket: "champions", role_id: opening.role_id).where("bucket_updated_on > ? ", 6.months.ago)
     filter_unique_candidates(candidates, count)
   end
 
   def recycle_icebox_for(recruiter, opening, count)
-    candidates = Candidate.where(bucket: "icebox", role_id: opening.role_id).where("bucket_updated_on < ? ", 6.months.ago)
+    candidates = Candidate.where(bucket: "icebox", role_id: opening.role_id).where("bucket_updated_on > ? ", 6.months.ago)
     filter_unique_candidates(candidates, count)
   end
 
   def recycle_recent_for(recruiter, opening, count)
-    candidates = Candidate.where(bucket: "recent", role_id: opening.role_id).order(created_at: :desc)
+    candidates = Candidate.where(bucket: "recent", role_id: opening.role_id).where("bucket_updated_on > ? ", 6.months.ago).order(created_at: :desc)
     filter_unique_candidates(candidates, count)
   end
 

1740fc506c7d96e2a9f9fc2cd8334c3989cf8f07
Aashish Dhawan <aashishdhawan@gmail.com>
2025-04-15 19:05:58 +0530

    Added progress bar

---
+++ app/helpers/progress_helper.rb
@@ -0,0 +1,27 @@
+module ProgressHelper
+  def display_campaign_progress(campaign)
+    candidates = campaign.candidates
+    total_candidates = candidates.count
+    waiting_candidates = candidates.where(status: ["waiting_for_evaluation"]).count
+    rejected_candidates = candidates.where(status: ["rejected_in_screening", "rejected_in_first_round", "rejected_in_second_round", "not_interested", "rejected_in_hr_round"]).count
+    pipelined_candidates = candidates.where(status: ["interview_to_be_scheduled", "first_round_scheduled", "second_round_scheduled", "hr_round_scheduled"]).count
+    selected_candidates = candidates.where(status: ["offer_to_be_made", "offer_made", "offer_accepted", "joined"]).count
+
+    not_contactable = candidates.where(status: ["on_hold", "no_show", "not_contactable", "follow_up_needed"]).count
+    html = ""
+    if total_candidates > 0
+      progress = ((waiting_candidates) / total_candidates.to_f) * 100
+      html << "<div class='bg-blue-400 h-1.5' style='width: #{progress}%'></div>"
+      progress = ((rejected_candidates) / total_candidates.to_f) * 100
+      html << "<div class='bg-red-400 h-1.5' style='width: #{progress}%'></div>"
+      progress = ((not_contactable) / total_candidates.to_f) * 100
+      html << "<div class='bg-sky-400 h-1.5' style='width: #{progress}%'></div>"
+
+      progress = ((pipelined_candidates) / total_candidates.to_f) * 100
+      html << "<div class='bg-yellow-400 h-1.5' style='width: #{progress}%'></div>"
+      progress = ((selected_candidates) / total_candidates.to_f) * 100
+      html << "<div class='bg-green-400 h-1.5' style='width: #{progress}%'></div>"
+    end
+    html.html_safe
+  end
+end

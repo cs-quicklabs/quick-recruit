e5c21e7b3bdc1b1369ee399c2775f10deb2fdabf
Aashish Dhawan <aashishdhawan@gmail.com>
2025-04-28 18:00:07 +0530

    champtions

--- app/controllers/opening/associations_controller.rb
+++ app/controllers/opening/associations_controller.rb
@@ -2,5 +2,6 @@ class Opening::AssociationsController < Opening::BaseController
   before_action :set_opening
 
   def index
+    @champions = @opening.candidates.where(bucket: :champions)
   end
 end

152d72a08a3ec8c123c68574175383fe93de2056
Aashish Dhawan <aashishdhawan@gmail.com>
2025-05-16 18:55:48 +0530

    hide inactive campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,6 @@
 class Campaign < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id", optional: false
   has_and_belongs_to_many :candidates
+
+  scope :active, -> { where(active: true) }
 end

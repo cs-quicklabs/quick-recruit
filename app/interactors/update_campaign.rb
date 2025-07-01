dd344ef9fd128530fe2a222510ba54d440aa482a
Aashish Dhawan <aashishdhawan@gmail.com>
2025-07-01 07:59:14 +0530

    remove candidate from sprint

--- app/interactors/update_campaign.rb
+++ app/interactors/update_campaign.rb
@@ -2,12 +2,12 @@ class UpdateCampaign < Patterns::Service
   def initialize(candidate, campaign_id, actor)
     @candidate = candidate
     @campaign = Campaign.find_by(id: campaign_id)
+
     @actor = actor
   end
 
   def call
     update_campaign
-    add_event
 
     candidate
   end
@@ -15,14 +15,22 @@ class UpdateCampaign < Patterns::Service
   private
 
   def update_campaign
-    candidate.campaigns.destroy_all
-    unless campaign.nil?
+    if campaign.nil?
+      add_event
+      candidate.campaigns.destroy_all
+    else
+      candidate.campaigns.destroy_all
       candidate.campaigns << campaign unless candidate.campaigns.include?(campaign)
+      add_event
     end
   end
 
   def add_event
-    Event.new(eventable: candidate, action: "update_campaign", action_for_context: "update_campaign", trackable: campaign, user: actor).save
+    if campaign.nil?
+      Event.new(eventable: candidate, action: "remove_campaign", action_for_context: "remove_campaign", trackable: candidate.campaigns.first, user: actor).save
+    else
+      Event.new(eventable: candidate, action: "update_campaign", action_for_context: "update_campaign", trackable: campaign, user: actor).save
+    end
   end
 
   attr_reader :candidate, :campaign, :actor

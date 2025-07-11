fc68793ce99b8fd0e13cedf6bc71ff8e1fc9c81d
Aashish Dhawan <aashishdhawan@gmail.com>
2025-07-11 17:20:12 +0530

    pagy size

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,7 +1,7 @@
 # Optionally override some pagy default with your own in the pagy initializer
 Pagy::DEFAULT[:limit] = 30 # items per page
 
-Pagy::DEFAULT[:size] = 9  # nav bar links
+Pagy::DEFAULT[:size] = 20  # nav bar links
 # Better user experience handled automatically
 require "pagy/extras/overflow"
 Pagy::DEFAULT[:overflow] = :last_page
